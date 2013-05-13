a8045b2db4ba2cff28065836abf1b5815132ff4d
Francois Berenger <berenger@riken.jp>
2013-05-13 14:09:34 +0900

    added license made the code more standard (type is t)

--- vector3.ml
+++ vector3.ml
@@ -1,25 +1,66 @@
+(* Copyright (c) 2013, Zhang Initiative Research Unit,
+ * Advance Science Institute, RIKEN
+ * 2-1 Hirosawa, Wako, Saitama 351-0198, Japan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+ * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. *)
 
-type vector3 = {x: float;
-                y: float;
-                z: float}
+type t = { x : float ;
+           y : float ;
+           z : float }
+
+(* constructors ------------------------------------------------------------ *)
 
 let origin =
   { x = 0. ;
     y = 0. ;
     z = 0. ; }
 
-let new_vector3 x y z =
+let make x y z =
   { x = x ;
     y = y ;
     z = z ; }
 
-let vector3_of_triplet (x, y, z) =
+let of_triplet (x, y, z) =
   { x = x ;
     y = y ;
     z = z ; }
 
-let triplet_of_vector3 v =
+let of_string str =
+  Scanf.sscanf str "%f %f %f"
+    (fun x y z -> { x = x ;
+                    y = y ;
+                    z = z })
+
+(* to other types ---------------------------------------------------------- *)
+
+let to_triplet v =
   (v.x, v.y, v.z)
+
+let to_string v =
+  Printf.sprintf "%f %f %f" v.x v.y v.z
+
+(* operations -------------------------------------------------------------- *)
 
 let dot v1 v2 =
   v1.x *. v2.x +.
@@ -44,16 +85,16 @@ let add v1 v2 =
     y = v1.y +. v2.y;
     z = v1.z +. v2.z }
 
-let squared_magnitude v =
+let mag2 v =
   v.x *. v.x +.
   v.y *. v.y +.
   v.z *. v.z
 
-let magnitude v =
-  sqrt (squared_magnitude v)
+let mag v =
+  sqrt (mag2 v)
 
-let distance v1 v2 =
-  magnitude (diff v1 v2)
+let dist v1 v2 =
+  mag (diff v1 v2)
 
 let div v s =
   { x = v.x /. s ;
@@ -66,20 +107,4 @@ let mult v s =
     z = v.z *. s }
 
 let normalize v =
-  div v (magnitude v)
-
-let vector3_of_string str =
-  match Scanf.sscanf str "%f %f %f" (fun x y z -> (x, y, z)) with
-    (x, y, z) -> { x = x ;
-                   y = y ;
-                   z = z }
-
-let string_of_vector3 v =
-  Printf.sprintf "%f %f %f" v.x v.y v.z
-
-(* H atom PDB line *)
-let make_pdb_line ?(bfactor=0.0) v =
-(* example:
-   'ATOM     10  C3  MPT    10     103.646  58.223 -36.577    1   0.07' *)
-  Printf.sprintf "ATOM          H  MPT      %12.3f%8.3f%8.3f%5d%7.2f"
-    v.x v.y v.z 1 bfactor
+  div v (mag v)
