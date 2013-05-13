b296241f951526b44704e8dfb324f7f900158329
Francois Berenger <berenger@riken.jp>
2013-05-13 14:14:29 +0900

    generated by ocamlc -i vector3.ml > vector3.mli

---
+++ vector3.mli
@@ -0,0 +1,18 @@
+type t = { x : float; y : float; z : float; }
+val origin : t
+val make : float -> float -> float -> t
+val of_triplet : float * float * float -> t
+val of_string : string -> t
+val to_triplet : t -> float * float * float
+val to_string : t -> string
+val dot : t -> t -> float
+val cross : t -> t -> t
+val angle : t -> t -> float
+val diff : t -> t -> t
+val add : t -> t -> t
+val mag2 : t -> float
+val mag : t -> float
+val dist : t -> t -> float
+val div : t -> float -> t
+val mult : t -> float -> t
+val normalize : t -> t
