4b70cef1e0bc6a9a6ea83022ac114f6c6f10111f
Francois Berenger <berenger@riken.jp>
2013-05-14 16:59:32 +0900

    moved qtest commands in test.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,3 @@ oasis setup
 ocaml setup.ml -configure
 ocaml setup.ml -build
 ocaml setup.ml -install
-
-# # commands for qtest
-# qtest -o test.ml extract vector3.ml
-# ocamlbuild -cflags -warn-error,+26 -use-ocamlfind -pkg oUnit,QTest2Lib \
-# test.native
-# ./test.native
