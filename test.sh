6b480d7739b679552cf6f52c5461305d01abdf25
Francois Berenger <berenger@riken.jp>
2013-05-15 13:33:00 +0900

    this is now handled via oasis

--- test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# commands for qtest
-qtest -o test.ml extract vector3.ml
-ocamlbuild -classic-display -cflags -warn-error,+26 -use-ocamlfind -pkg oUnit,QTest2Lib test.native
-./test.native
