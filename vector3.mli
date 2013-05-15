57559f5439f6a04c248f41bb91e62c6286ae9795
Francois Berenger <berenger@riken.jp>
2013-05-15 11:24:36 +0900

    regenerated by ocamlc -i vector3.ml > vector3.mli

--- vector3.mli
+++ vector3.mli
@@ -7,11 +7,11 @@ val to_triplet : t -> float * float * float
 val to_string : t -> string
 val dot : t -> t -> float
 val cross : t -> t -> t
-val angle : t -> t -> float
 val diff : t -> t -> t
 val add : t -> t -> t
 val mag2 : t -> float
 val mag : t -> float
+val angle : t -> t -> float
 val dist : t -> t -> float
 val div : t -> float -> t
 val mult : t -> float -> t
