e2db4c3eb9781e1fdbc518d7f1f13348d25cc791
Francois BERENGER <francois.berenger@inria.fr>
2016-10-17 11:33:31 +0200

    simplified build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 #set -x
 
 oasis setup
-ocaml setup.ml -configure --enable-tests --prefix `opam config var prefix`
+ocaml setup.ml -configure --prefix `opam config var prefix`
 ocaml setup.ml -build
-./test.native && ocaml setup.ml -install
+ocaml setup.ml -install
