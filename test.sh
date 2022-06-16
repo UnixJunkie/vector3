368c037bb973e51a784b02b4ac6a532ba8e3aac7
Francois Berenger <berenger@edu.k.u-tokyo.ac.jp>
2022-06-16 14:24:40 +0200

    updated test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 # extract test code then run it
 \rm -f test.native
-qtest -o test.ml extract vector3.ml
+qtest -o test.ml extract src/vector3.ml
 ocamlbuild test.native -pkgs vector3,oUnit,qcheck
 ./test.native
